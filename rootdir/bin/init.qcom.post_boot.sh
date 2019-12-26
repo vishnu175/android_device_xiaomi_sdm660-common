d459a5f60192218d7fad8046a8d4dec484c6660f
Dyneteve <dyneteve@pixelexperience.org>
2019-12-26 18:42:39 +0000

    sdm660-common: Update scheduler settings and cpusets for sdm660

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2807,38 +2807,40 @@ case "$target" in
                   ;;
             esac
 
-      # Core control parameters on silver
-      echo 0 0 0 0 1 1 > /sys/devices/system/cpu/cpu0/core_ctl/not_preferred
-      echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
-      echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
-      echo 40 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
-      echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
-      echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster
-      echo 8 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
+      # Core control parameters for big cluster
+      echo 2 > /sys/devices/system/cpu/cpu6/core_ctl/min_cpus
+      echo 60 > /sys/devices/system/cpu/cpu6/core_ctl/busy_up_thres
+      echo 30 > /sys/devices/system/cpu/cpu6/core_ctl/busy_down_thres
+      echo 100 > /sys/devices/system/cpu/cpu6/core_ctl/offline_delay_ms
+      echo 4 > /sys/devices/system/cpu/cpu6/core_ctl/task_thres
+
+      # Disable Core control on silver
+      echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/enable
 
       # Setting b.L scheduler parameters
-      echo 96 > /proc/sys/kernel/sched_upmigrate
-      echo 90 > /proc/sys/kernel/sched_downmigrate
-      echo 140 > /proc/sys/kernel/sched_group_upmigrate
-      echo 120 > /proc/sys/kernel/sched_group_downmigrate
+      echo 95 95 > /proc/sys/kernel/sched_upmigrate
+      echo 85 85 > /proc/sys/kernel/sched_downmigrate
+      echo 100 > /proc/sys/kernel/sched_group_upmigrate
+      echo 10 > /proc/sys/kernel/sched_group_downmigrate
       echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
       # configure governor settings for little cluster
       echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
       echo 0 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/rate_limit_us
       echo 1209600 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
+      echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/pl
       echo 576000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
       # configure governor settings for big cluster
       echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
       echo 0 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/rate_limit_us
       echo 1344000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_freq
+      echo 1 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/pl
       echo 652800 > /sys/devices/system/cpu/cpu6/cpufreq/scaling_min_freq
 
-      # sched_load_boost as -6 is equivalent to target load as 85. It is per cpu tunable.
-      echo -6 >  /sys/devices/system/cpu/cpu6/sched_load_boost
-      echo -6 >  /sys/devices/system/cpu/cpu7/sched_load_boost
-      echo 85 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_load
+      # Disable wsf, beacause we are using efk.
+      # wsf Range : 1..1000 So set to bare minimum value 1.
+      echo 1 > /proc/sys/vm/watermark_scale_factor
 
       echo "0:1209600" > /sys/module/cpu_boost/parameters/input_boost_freq
       echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
@@ -2898,8 +2900,8 @@ case "$target" in
             echo N > /sys/module/lpm_levels/L3/cpu7/ret/idle_enabled
 
             # cpuset parameters
-            echo 0-5 > /dev/cpuset/background/cpus
-            echo 0-5 > /dev/cpuset/system-background/cpus
+            echo 0-3 > /dev/cpuset/background/cpus
+            echo 0-3 > /dev/cpuset/system-background/cpus
 
             # Turn off scheduler boost at the end
             echo 0 > /proc/sys/kernel/sched_boost
