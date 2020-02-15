5535c505ddc2dfe6e39c4f527092275927b6340d
Kuber Sharma <kubersharma001@gmail.com>
2020-02-15 12:27:34 +0530

    sdm660-common: Add debug WFC prop to force enable it.

--- common_prop.mk
+++ common_prop.mk
@@ -183,6 +183,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.vendor.ims.disableUserAgent=0
+    persist.dbg.wfc_avail_ovr=1 \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
